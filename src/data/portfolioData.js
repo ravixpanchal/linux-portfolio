b190b407ad0526a08a6262cc3069fc3457becc30
ravixpanchal <ravi.panchal.kaithi@gmail.com>
2026-09-06 03:26:59 +0530

    Update resume Google Drive URL

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -9,7 +9,7 @@ export const portfolioData = {
     github: "https://github.com/ravixpanchal",
     linkedin: "https://www.linkedin.com/in/ravixpanchal/",
     linktree: "https://linktr.ee/ravi.panchal",
-    resumeUrl: "https://drive.google.com/file/d/1AHDVBgfkWsYFV7-XVrv1GK4BgdhAxERD/view?usp=sharing",
+    resumeUrl: "https://drive.google.com/file/d/1UM9YVmchOxK16GuE0cifRuuOJot-Ju35/view?usp=sharing",
     avatar: "/avatar.jpg",
     status: "🟢 Available for Full-time Roles & Internships",
     bio: "Passionate AI/ML engineer focused on building high-performance scalable web applications, RAG pipelines, and automated data solutions."
